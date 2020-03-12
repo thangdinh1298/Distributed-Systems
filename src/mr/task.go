32dc82387b76a96de5fc971944bcb7d98afddf4d
thangdinh1298 <thangdinh.umbala@gmail.com>
2020-03-12 11:36:45 +0700

    Added time dispatched field to task to track how long task has been dispatched

--- src/mr/task.go
+++ src/mr/task.go
@@ -1,17 +1,28 @@
 package mr
 
+import "time"
+
 type taskType int
 
 const (
 	mapTask    taskType = 0
 	reduceTask taskType = 1
+	waitTask   taskType = 2
 )
 
 // Task interface represents a task
 type Task struct {
-	TType   taskType
-	Args    []string
-	TaskNum int
-	NReduce int
-	NMap    int //number of map tasks
+	TType          taskType
+	Args           []string
+	TaskNum        int
+	NReduce        int
+	NMap           int       //number of map tasks
+	TimeDispatched time.Time //time that the task was last dispatched
+}
+
+type ErrOutOfTask struct {
+}
+
+func (e ErrOutOfTask) Error() string {
+	return "No more tasks to perform"
 }
